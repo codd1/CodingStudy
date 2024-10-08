9c83c92af97e93f63ade71abc4bf402983e8c757
unknown <mgs08041@gmail.com>
2024-10-08 23:58:19 +0900

    세그먼트 트리 - 구간 합 구하기(2042번)

--- codd1/Tree/2042.cpp
+++ codd1/Tree/2042.cpp
@@ -4,6 +4,17 @@
 
 using namespace std;
 
+void initializeTree(vector<int>& tree, int tree_size, int num1, int num2) {
+	if (num1 != 0) {		// 맨 처음 함수 호출할 때 num1 == 0이기 때문
+		tree[tree_size / 2 - 1] = num2;
+	}
+
+	// 구간 합: tree[N] = tree[2N] + tree[2N + 1]
+	for (int i = tree_size / 2 - 1; i >= 0; i--) {
+		tree[i] = tree[i * 2] + tree[i * 2 + 1];
+	}
+}
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
@@ -24,7 +35,59 @@ int main() {
 	}
 
 	int tree_size = pow(2, i) * 2;
-	vector<int> tree(tree_size);
+	vector<int> tree(tree_size, 0);
+
+	i = tree_size / 2;
+	for (int j = 0; j < N; j++, i++) {
+		cin >> tree[i];
+	}
+
+	initializeTree(tree, tree_size, 0, 0);
+
+	int type1_count = 0;		// M번이 되면 종료
+	int type2_count = 0;		// K번이 되면 종료
+	
+	int type;	// 1 또는 2
+	int num1, num2;
+	int start, end;
+	vector<int> result;
+	int sum = 0;
+
+	while (type1_count < M || type2_count < K) {
+		cin >> type >> num1 >> num2;
+
+		if (type == 1) {
+			initializeTree(tree, tree_size, num1, num2);
+			type1_count++;
+		}
+		else if (type == 2) {
+			start = num1 + (tree_size / 2 - 1);
+			end = num2 + (tree_size / 2 - 1);
+
+			while (start <= end) {
+				if (start % 2 == 1) {
+					result.push_back(start);
+				}
+
+				if (end % 2 == 0) {
+					result.push_back(end);
+				}
+
+				start = (start + 1) / 2;
+				end = (end - 1) / 2;
+			}
+
+			for (int i = 0; i < result.size(); i++) {
+				sum += tree[result[i]];
+			}
+
+			cout << sum << "\n";
+			type2_count++;
+
+			sum = 0;
+			fill(result.begin(), result.end(), 0);
+		}
+	}
 
 	return 0;
 }
