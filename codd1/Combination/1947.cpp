9d78ddc98530106602e2f30ea3710f59eeab4bec
unknown <mgs08041@gmail.com>
2024-10-17 01:27:44 +0900

    순열(완전 순열) - 선물 전달(1947번) / 성공

--- codd1/Combination/1947.cpp
+++ codd1/Combination/1947.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 
+#define REMAINDER 1'000'000'000
+
 using namespace std;
 
 int main() {
@@ -11,15 +13,16 @@ int main() {
 	int N;
 	cin >> N;
 
-	vector<int> D(N + 1);
-	D[1] = 1;
-	D[2] = 1;		// 사람이 1명일 경우, 경우의 수는 본인이 하나도 받지 못 하는 경우 1개이다.
+	vector<long long> D(N + 1);
+	D[1] = 1;		// 사람이 1명일 경우, 경우의 수는 본인이 하나도 받지 못 하는 경우 1개이다.
+	D[2] = 1;		// 사람이 2명일 경우, 경우의 수는 서로 선물을 교환하는 경우 1개이다.
+	D[3] = 2;		// 사람이 3명일 경우, 경우의 수는 B A C / C A B 2개이다.
 
-	for (int i = 2; i <= N; i++) {
-		D[i] = (i - 1) * (D[i - 1] + D[i - 2]);
+	for (int i = 4; i <= N; i++) {
+		D[i] = (i - 1) * ((D[i - 1] % REMAINDER) + (D[i - 2] % REMAINDER) % REMAINDER);		// 점화식
 	}
 
-	D;
+	cout << D[N] % REMAINDER;
 
 	return 0;
 }
